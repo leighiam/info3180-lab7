f474f67789f2c992f6daba3dbaf192b51e2d3e17
Ashleigh Barker <barkerashleigh1@gmail.com>
2020-05-07 19:41:58 -0500

    Form completed

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 01:18:38 2020
+
+@author: ashleigh"""
+
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms.validators import DataRequired
+
+
+class UploadForm(FlaskForm):
+    description = TextAreaField('Description', validators=[DataRequired()])
+    photo = FileField('Photo', validators=[
+        FileRequired(),
+        FileAllowed(['jpg', 'png'], 'Images only!')
+    ])
 
